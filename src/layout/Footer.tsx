f22fe2b5eabd3bec98f72482beea702f64aeee0a
Erfan-Karimipour <programming1402@gmail.com>
2023-06-04 18:35:50 +0330

    About Us added

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,20 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { ContactUs } from '../sharedComponents/Footer/ContactUs'
+import { AboutUs } from '../sharedComponents/Footer/AboutUs'
 
 export const Footer = () => {
   return (
-    <div className='mt-8'>
-      <div className='bg-gray-100 w-full flex flex-col items-center mb-2 text-center py-5'>
-        <p className='font-bold my-2'>تماس با ما</p>
-        <div className='flex flex-col items-center w-full'>
-          <p className='flex items-center'>
-            <p>Programming1402@gmail.com</p>
-            <ion-icon name="mail-outline" class="mx-2"></ion-icon>
-          </p>
-          <p className='flex items-center'>
-            <p>09555555533</p>
-            <ion-icon name="call-outline" class="mx-2"></ion-icon>
-          </p>
-        </div>
+    <div className='mt-8 bg-gray-100'>
+      <div className='grid grid-cols-2 text-center py-5 w-fit m-auto mb-2 gap-48'>
+        <ContactUs  />
+        <AboutUs    />
       </div>
       <div className='text-red-600 bg-black py-1 text-center'> E Corp 2023 ©</div>
     </div>
