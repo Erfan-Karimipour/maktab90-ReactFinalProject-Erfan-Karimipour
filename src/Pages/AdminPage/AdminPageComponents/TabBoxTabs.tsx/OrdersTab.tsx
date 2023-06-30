a401d8fa9be193f4b2f2fd5dc6477488febcfdf8
Erfan-Karimipour <programming1402@gmail.com>
2023-06-30 13:08:11 +0330

    Order completed, finished

--- src/Pages/AdminPage/AdminPageComponents/TabBoxTabs.tsx/OrdersTab.tsx
+++ src/Pages/AdminPage/AdminPageComponents/TabBoxTabs.tsx/OrdersTab.tsx
@@ -1,12 +1,15 @@
-import  React, { useState } from 'react';
+import  React, { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import axios from 'axios';
 import { Button, IconButton, ThemeProvider, createTheme } from '@mui/material';
 import { StyledButton } from '@nextui-org/react';
 import { blue } from '@mui/material/colors';
 import { RadioInputs } from './OrdersTabComponents/RadioInputs';
+import { useData } from '../../../../Context/Context';
 
 export function OrdersTab() {
+
+  let {updateList, setUpdateList} = useData();
 
   let [showCompleted, setShowCompleted] = useState('all');
 
@@ -43,7 +46,7 @@ export function OrdersTab() {
         setRowCount(total);
         setLoading(false);
       });
-  }, [paginationModel.page, paginationModel.pageSize, showCompleted]);
+  }, [paginationModel.page, paginationModel.pageSize, showCompleted, updateList, setUpdateList]);
 
   const theme = createTheme({
     palette: {
@@ -58,7 +61,35 @@ export function OrdersTab() {
   })
 
   const today = new Date();
+
+  let [namesAndPrices, setNamesAndPrices] = useState([])
   
+  function nameAndPriceGetter(prod) {
+    setNamesAndPrices([])
+    prod.products.map((product) => {
+      axios.get(`http://localhost:8000/api/products/${product.product}`).then((res) => {
+
+        let newData = {
+          name  : res.data.data.product.name,
+          price : res.data.data.product.price,
+        }
+        setNamesAndPrices([...namesAndPrices, newData]);
+
+        // product = {...product, name: res.data.data.product.name, price: res.data.data.product.price}
+
+        // console.log(namesAndPrices);
+        
+        // setNamesAndPrices(namesAndPrices.push(newData));     
+
+        
+      }).catch((err) => {
+        console.log(err);
+      })
+    })
+    
+  }
+  
+
 
   return (
 
@@ -89,6 +120,7 @@ export function OrdersTab() {
           { field: 'id', headerName: <p className='text-xl' style={{fontFamily: 'vazir'}}>اطلاعات بیشتر</p>, flex: 1, 
             renderCell: (params) => (
             <button className='mr-10 text-3xl bg-blue-500 rounded-full flex text-white' onClick={() => {
+              nameAndPriceGetter(params.row);
               setModal(params.row);
             }}>
               <ion-icon name="alert-circle-outline"></ion-icon>
@@ -109,10 +141,10 @@ export function OrdersTab() {
         <RadioInputs showCompleted={setShowCompleted}/>
       </ThemeProvider>
 
-      {modal && 
+      {modal &&
       <div className='top-0 bottom-0 left-0 right-0 absolute flex z-10 bg-black bg-opacity-25'>
         <div className='bg-white w-1/3 flex h-fit m-auto self-center flex-col rounded-md p-5'>
-          <button className='text-white bg-red-500 rounded-full flex w-fit text-2xl' onClick={() => setModal(``)}>
+          <button className='text-white bg-red-500 rounded-full flex w-fit text-2xl' onClick={() => {setModal(``); }}>
             <ion-icon name="close-circle-outline"></ion-icon>
           </button>
           <p className='w-full text-center text-3xl pb-3 border-b'>اطلاعات</p>
@@ -142,7 +174,7 @@ export function OrdersTab() {
             <p>{modal.createdAt}</p>
           </div>
           
-          <div className='w-full h-96 overflow-y-scroll px-3'>
+          <div className='w-full h-96 overflow-y-scroll px-3 mt-4'>
             <table className='w-full'>
               <thead>
                 <tr className='border-b'>
@@ -152,26 +184,34 @@ export function OrdersTab() {
                 </tr>
               </thead>
                 {
-                  modal.products.map((product) => {
-                      
-                    return(
-                    <tr className='border-b'>
-                      <td className='w-2/3 border'>{product.product}</td>
-                      <td className='w-1/6 border'>price</td>
-                      <td className='w-1/6 border'>{product.count}</td>
-                    </tr>
-                    )
+                  modal.products.map((product, index) => {
 
+                    if(namesAndPrices.length >= 1){
+
+                      console.log(product);
+                      
+                      return(
+                        <tr className='border-b'>
+                        <td className='w-2/3 border'>{namesAndPrices[index].name}</td>
+                        <td className='w-1/6 border'>{namesAndPrices[index].price}</td>
+                        <td className='w-1/6 border'>{product.count}</td>
+                      </tr>
+                    )
+                    
+                  }
+                    
                   })
                 }
             </table>
           </div>
             {
-              modal.deliveryStatus == false ? <button className='text-center bg-green-500 rounded-md w-full text-white' onClick={() => {
+              modal.deliveryStatus == false ? <button className='text-center bg-green-500 rounded-md w-full text-white py-2' onClick={() => {
                 axios.patch(`http://localhost:8000/api/orders/${modal._id}`, {
                   deliveryStatus: true,
-                  deliveryDate  : today 
+                  deliveryDate  : today
                 });
+                setUpdateList(!updateList);
+                setModal(``);
               }}>تحویل شد</button> :
               <p className='text-center'>تحویل شده در {modal.deliveryDate}</p>
             }
